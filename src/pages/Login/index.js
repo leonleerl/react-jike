3840f4d4ec857ee8b912920bc4fbb78d1a2a17b0
leonleerl <leon.runlongli@gmail.com>
2024-08-02 09:27:51 +0800

    实现表单校验和数据收集

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,52 @@
+import "./index.scss";
+import { Card, Form, Input, Button, message } from "antd";
+import logo from "@/assets/logo.png";
+
 const Login = () => {
+  const onFinish = (values) => {
+    console.log(values.mobile);
+    console.log(values.code);
+  };
   return (
-    <>
-      <div>This is login</div>
-    </>
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form onFinish={onFinish} validateTrigger="onBlur">
+          <Form.Item
+            name="mobile"
+            rules={[
+              {
+                required: true,
+                message: "请输入手机号",
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: "请输入正确的手机号格式",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[
+              {
+                required: true,
+                message: "请输入验证码",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
   );
 };
 
