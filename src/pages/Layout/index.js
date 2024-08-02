2e61583f625d6da55d493a001504bbf8c7051eda
leonleerl <leon.runlongli@gmail.com>
2024-08-02 14:38:51 +0800

    根据路由路径高亮左侧菜单

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -6,7 +6,7 @@ import {
   LogoutOutlined,
 } from "@ant-design/icons";
 import "./index.scss";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 const { Header, Sider } = Layout;
 
@@ -31,10 +31,16 @@ const items = [
 const GeekLayout = () => {
   const navigate = useNavigate();
   const onMenuClick = (route) => {
-    console.log("click--" + route);
+    // console.log("click--" + route);
     const path = route.key;
     navigate(path);
   };
+
+  // 反向高亮
+  // 1.获取当前路由路径
+  const location = useLocation();
+  console.log(location.pathname);
+  const selectedKey = location.pathname;
   return (
     <Layout>
       <Header className="header">
@@ -53,7 +59,7 @@ const GeekLayout = () => {
           <Menu
             mode="inline"
             theme="dark"
-            defaultSelectedKeys={["1"]}
+            selectedKeys={selectedKey}
             items={items}
             style={{ height: "100%", borderRight: 0 }}
             onClick={onMenuClick}
