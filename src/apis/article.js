8818ab2f67f787c1a6e46bae35328b627d8e7f7b
leonleerl <leon.runlongli@gmail.com>
2024-08-03 12:46:51 +0800

    富文本编辑器+频道列表渲染+收集表单并POST提交

---
+++ src/apis/article.js
@@ -0,0 +1,19 @@
+// 封装和文章相关的接口函数
+const { request } = require("@/utils");
+
+// 获取频道列表请求
+export function getChannelAPI() {
+  return request({
+    url: "/channels",
+    method: "GET",
+  });
+}
+
+// 提交文章表单
+export function createArticleAPI(data) {
+  return request({
+    url: "mp/articles?draft=false",
+    method: "POST",
+    data,
+  });
+}
--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -12,10 +12,40 @@ import {
 import { PlusOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import "./index.scss";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useEffect, useState } from "react";
+import { createArticleAPI, getChannelAPI } from "@/apis/article";
 
 const { Option } = Select;
 
 const Publish = () => {
+  // 获取频道列表
+  const [channelList, setChannelList] = useState([]);
+  useEffect(() => {
+    // 封装函数，在函数体内调用接口
+    const getChannelList = async () => {
+      const res = await getChannelAPI();
+      setChannelList(res.data.channels);
+    };
+    getChannelList();
+  }, []);
+  // 提交表单
+  const onFinish = (formValue) => {
+    console.log(formValue);
+    const { title, content, channel_id } = formValue;
+    // 处理收集到的表单数据
+    const reqData = {
+      title: title,
+      content: content,
+      cover: {
+        type: 0,
+        images: [],
+      },
+      channel_id: channel_id,
+    };
+    createArticleAPI(reqData);
+  };
   return (
     <div className="publish">
       <Card
@@ -32,6 +62,7 @@ const Publish = () => {
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 16 }}
           initialValues={{ type: 1 }}
+          onFinish={onFinish}
         >
           <Form.Item
             label="标题"
@@ -46,7 +77,16 @@ const Publish = () => {
             rules={[{ required: true, message: "请选择文章频道" }]}
           >
             <Select placeholder="请选择文章频道" style={{ width: 400 }}>
-              <Option value={0}>推荐</Option>
+              {/* {channelList.map((item) => (
+                <Option key={item.id} value={item.id}>
+                  {item.name}
+                </Option>
+              ))} */}
+              {channelList.map((item) => (
+                <Option key={item.id} value={item.id}>
+                  {item.name}
+                </Option>
+              ))}
             </Select>
           </Form.Item>
           <Form.Item
@@ -55,6 +95,11 @@ const Publish = () => {
             rules={[{ required: true, message: "请输入文章内容" }]}
           >
             {/* 富文本编辑器 */}
+            <ReactQuill
+              className="publish-quill"
+              theme="snow"
+              placeholder="请输入文章内容"
+            />
           </Form.Item>
 
           <Form.Item wrapperCol={{ offset: 4 }}>
