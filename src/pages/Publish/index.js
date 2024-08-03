106384d7c7d0966c5368024bef5d199fc2e2c2ec
leonleerl <leon.runlongli@gmail.com>
2024-08-03 18:21:57 +0800

    上传文章封面+控制数量

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -46,6 +46,17 @@ const Publish = () => {
     };
     createArticleAPI(reqData);
   };
+  // 上传回调
+  const [imageList, setImageList] = useState([]);
+  const onChange = (value) => {
+    console.log("正在上传图片");
+    setImageList(value.fileList);
+  };
+  // 切换图片封面类型
+  const [imageType, setImageType] = useState(0);
+  const onTypeChange = (e) => {
+    setImageType(e.target.value);
+  };
   return (
     <div className="publish">
       <Card
@@ -61,7 +72,7 @@ const Publish = () => {
         <Form
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 16 }}
-          initialValues={{ type: 1 }}
+          initialValues={{ type: 0 }}
           onFinish={onFinish}
         >
           <Form.Item
@@ -88,6 +99,29 @@ const Publish = () => {
                 </Option>
               ))}
             </Select>
+          </Form.Item>
+          <Form.Item label="封面">
+            <Form.Item name="type">
+              <Radio.Group onChange={onTypeChange}>
+                <Radio value={1}>单图</Radio>
+                <Radio value={3}>三图</Radio>
+                <Radio value={0}>无图</Radio>
+              </Radio.Group>
+            </Form.Item>
+            {imageType > 0 && (
+              <Upload
+                listType="picture-card"
+                showUploadList
+                action={"http://geek.itheima.net/v1_0/upload"}
+                name="image"
+                maxCount={imageType > 0 ? imageType : 0}
+                onChange={onChange}
+              >
+                <div style={{ marginTop: 8 }}>
+                  <PlusOutlined />
+                </div>
+              </Upload>
+            )}
           </Form.Item>
           <Form.Item
             label="内容"
